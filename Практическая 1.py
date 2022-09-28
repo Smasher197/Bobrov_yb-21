49d822f85a6d1eb86cfedf23aadf507f15600407
Smasher197 <114362997+Smasher197@users.noreply.github.com>
2022-09-28 20:57:26 +0300

    Add files via upload

---
+++ Практическая 1.py
@@ -0,0 +1,15 @@
+print("Ваша фамилия?")
+surname = input()
+
+print("Ваше имя?")
+name = input()
+
+print("Сколько вам лет?")
+age = int(input())
+
+print("Где вы живете?")
+live = input()
+
+print("Ваши фамилия:", surname, ",", "имя:", name)
+print("Ваш возраст", age)
+print("Вы живете в", live)
